d13edc0e201ceb29ac40527de58d784b7d57e2a8
Kirthick Sakthi V <158608170+KirthickSakthiV@users.noreply.github.com>
2024-04-12 10:11:20 +0530

    Add files via upload

---
+++ Fade in-out/Fade.js
@@ -0,0 +1,9 @@
+function fadeIn() {
+    var element = document.getElementById('element');
+    element.style.opacity = '1';
+}
+
+function fadeOut() {
+    var element = document.getElementById('element');
+    element.style.opacity = '0';
+}
